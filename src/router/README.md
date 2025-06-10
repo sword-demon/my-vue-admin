68338d485dfc40c5235627e07cac76758ba0e7d7
王鑫123wangxin <1418667580@qq.com>
2025-06-10 23:16:57 +0800

    wx, docs: token 的验证方式

--- src/router/README.md
+++ src/router/README.md
@@ -10,3 +10,5 @@ router.beforeEach(() => {});
 ```
 
 用户登录之后会有一个 token,如果有 token 代表登录了,没有的话就代表没有登录
+这里无需管 token 的值是不是真的 token,因为每次都会携带 token 请求后端接口
+后端接口会进行验证这个 token 合不合法
--- src/router/guard.ts
+++ src/router/guard.ts
@@ -6,7 +6,8 @@ import { useUserStore } from "@/store/auth";
 router.beforeEach((to) => {
   const userSore = useUserStore();
 
-  const isLogin = userSore.token;
+  const isLogin = userSore.token; // 这里验证了一个值存不存在,前端不会知道这个 token 是不是真的 token
+  // 每次发送请求的时候,后端还是会进行审核这个 token 的
   if (!isLogin) {
     // 未登录
     // 如果你的是登录页,那么就不处理
