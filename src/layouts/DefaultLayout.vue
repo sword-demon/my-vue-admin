6c1ddb470754c66069d395623e08394656407186
王鑫123wangxin <1418667580@qq.com>
2025-06-11 22:33:51 +0800

    wx, feat: 基本布局页面以及样式调整

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,3 +1,31 @@
 <template>
-    <div>我是主页面</div>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+// 左侧菜单栏宽度撑满屏幕
+.el-aside {
+  height: 100vh;
+  background-color: white;
+  box-shadow: 10px 0 8px -2px rgba(0, 0, 0, 0.2);
+}
+
+.el-header {
+  padding: 0 !important;
+}
+
+// 添加固定高度,不能任由内容撑开
+// 超出部分以滚动条的形式展示
+.el-main {
+  height: 80vh;
+  overflow: auto;
+}
+</style>
